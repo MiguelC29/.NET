d1504b961d9eeb4c7adf494c98bba0fbb55ef707
MiguelC29 <miguelito28c@gmail.com>
2024-04-22 11:37:40 -0500

    App de videojuegos

---
+++ VideoGames/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace VideoGames
+{
+    partial class CellGames
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtDisponible = new System.Windows.Forms.TextBox();
+            this.txtTamano = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbJuego = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnMasVendido = new System.Windows.Forms.Button();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.btnFinalizarVenta = new System.Windows.Forms.Button();
+            this.pbJuego = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbJuego)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFinalizarVenta);
+            this.groupBox1.Controls.Add(this.btnVender);
+            this.groupBox1.Controls.Add(this.btnComprar);
+            this.groupBox1.Controls.Add(this.pbJuego);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtDisponible);
+            this.groupBox1.Controls.Add(this.txtTamano);
+            this.groupBox1.Controls.Add(this.txtCategoria);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbJuego);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox1.Location = new System.Drawing.Point(56, 157);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(476, 299);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Juegos";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(149, 164);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.ReadOnly = true;
+            this.txtPrecio.Size = new System.Drawing.Size(129, 22);
+            this.txtPrecio.TabIndex = 10;
+            // 
+            // txtDisponible
+            // 
+            this.txtDisponible.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisponible.Location = new System.Drawing.Point(149, 192);
+            this.txtDisponible.Name = "txtDisponible";
+            this.txtDisponible.ReadOnly = true;
+            this.txtDisponible.Size = new System.Drawing.Size(129, 22);
+            this.txtDisponible.TabIndex = 9;
+            // 
+            // txtTamano
+            // 
+            this.txtTamano.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTamano.Location = new System.Drawing.Point(149, 136);
+            this.txtTamano.Name = "txtTamano";
+            this.txtTamano.ReadOnly = true;
+            this.txtTamano.Size = new System.Drawing.Size(129, 22);
+            this.txtTamano.TabIndex = 8;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(149, 77);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(129, 22);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(13, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Disponibles:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(13, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Precio:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(13, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tamaño en KB:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(13, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Categoría:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(13, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // cmbJuego
+            // 
+            this.cmbJuego.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbJuego.FormattingEnabled = true;
+            this.cmbJuego.Location = new System.Drawing.Point(16, 30);
+            this.cmbJuego.Name = "cmbJuego";
+            this.cmbJuego.Size = new System.Drawing.Size(401, 24);
+            this.cmbJuego.TabIndex = 0;
+            this.cmbJuego.SelectedIndexChanged += new System.EventHandler(this.cmbJuego_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnMasVendido);
+            this.groupBox2.Controls.Add(this.btnBuscar);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox2.Location = new System.Drawing.Point(553, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 299);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opciones";
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnComprar.FlatAppearance.BorderSize = 3;
+            this.btnComprar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComprar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnComprar.Location = new System.Drawing.Point(55, 251);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(75, 24);
+            this.btnComprar.TabIndex = 12;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
+            // 
+            // btnVender
+            // 
+            this.btnVender.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnVender.FlatAppearance.BorderSize = 3;
+            this.btnVender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVender.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnVender.Location = new System.Drawing.Point(166, 251);
+            this.btnVender.Name = "btnVender";
+            this.btnVender.Size = new System.Drawing.Size(75, 24);
+            this.btnVender.TabIndex = 13;
+            this.btnVender.Text = "Vender";
+            this.btnVender.UseVisualStyleBackColor = true;
+            this.btnVender.Click += new System.EventHandler(this.btnVender_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBuscar.Location = new System.Drawing.Point(48, 70);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(111, 40);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar Juego";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnMasVendido
+            // 
+            this.btnMasVendido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMasVendido.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnMasVendido.Location = new System.Drawing.Point(29, 157);
+            this.btnMasVendido.Name = "btnMasVendido";
+            this.btnMasVendido.Size = new System.Drawing.Size(149, 53);
+            this.btnMasVendido.TabIndex = 1;
+            this.btnMasVendido.Text = "Juego Más Vendido";
+            this.btnMasVendido.UseVisualStyleBackColor = true;
+            this.btnMasVendido.Click += new System.EventHandler(this.btnMasVendido_Click);
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoria.Location = new System.Drawing.Point(149, 108);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.ReadOnly = true;
+            this.txtCategoria.Size = new System.Drawing.Size(129, 22);
+            this.txtCategoria.TabIndex = 7;
+            // 
+            // btnFinalizarVenta
+            // 
+            this.btnFinalizarVenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizarVenta.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnFinalizarVenta.Location = new System.Drawing.Point(319, 251);
+            this.btnFinalizarVenta.Name = "btnFinalizarVenta";
+            this.btnFinalizarVenta.Size = new System.Drawing.Size(116, 33);
+            this.btnFinalizarVenta.TabIndex = 14;
+            this.btnFinalizarVenta.Text = "Finalizar Venta";
+            this.btnFinalizarVenta.UseVisualStyleBackColor = true;
+            this.btnFinalizarVenta.Click += new System.EventHandler(this.btnFinalizarVenta_Click);
+            // 
+            // pbJuego
+            // 
+            this.pbJuego.Location = new System.Drawing.Point(301, 71);
+            this.pbJuego.Name = "pbJuego";
+            this.pbJuego.Size = new System.Drawing.Size(145, 166);
+            this.pbJuego.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbJuego.TabIndex = 11;
+            this.pbJuego.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VideoGames.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(56, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 118);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CellGames
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(767, 471);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "CellGames";
+            this.Text = "CellGames";
+            this.Load += new System.EventHandler(this.CellGames_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbJuego)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtDisponible;
+        private System.Windows.Forms.TextBox txtTamano;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbJuego;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pbJuego;
+        private System.Windows.Forms.Button btnVender;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.Button btnMasVendido;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button btnFinalizarVenta;
+    }
+}
+
---
+++ VideoGames/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VideoGames
+{
+    public partial class CellGames : Form
+    {
+        public CellGames()
+        {
+            InitializeComponent();
+        }
+
+        String[,] matrizJuegos =
+        {
+            {"FIFA", "DEPORTES", "100", "100000", "50"},
+            {"NFS", "VELOCIDAD", "120", "120000", "10"},
+            {"HALO", "ACCIÓN", "150", "160000", "100"},
+            {"MINECRAFT", "AVENTURA", "180", "200000", "50"}
+        };
+
+        // [,] -> doble dimension | [] -> vector normal
+        int[] vectorVentas = { 0, 0, 0, 0 };
+        int descuento = 0, totalVentas, index;
+
+        private void btnComprar_Click(object sender, EventArgs e)
+        {
+            int cantidad = 0;
+            try
+            {
+                cantidad = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Ingrese la cantidad que desea " + "comprar", "Comprar Juegos", "0"));
+            } catch(Exception ex)
+            {
+                MessageBox.Show("No ingresó ninguna cantidad");
+            }
+
+            matrizJuegos[index, 4] = (int.Parse(matrizJuegos[index, 4]) + cantidad).ToString();
+            txtDisponible.Text = matrizJuegos[index, 4];
+        }
+
+        private void btnVender_Click(object sender, EventArgs e)
+        {
+            btnComprar.Enabled = false;
+            btnFinalizarVenta.Enabled = true;
+            int cantidad = 0;
+            try
+            {
+                cantidad = int.Parse(Microsoft.VisualBasic.Interaction.InputBox("Ingrese la cantidad que desea " + "vender", "Vender Juegos", "0"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No ingresó ninguna cantidad");
+            }
+
+            if (cantidad <= int.Parse(matrizJuegos[index, 4]))
+            {
+                matrizJuegos[index, 4] = (int.Parse(matrizJuegos[index, 4]) - cantidad).ToString();
+                txtDisponible.Text = matrizJuegos[index, 4];
+                vectorVentas[index] += cantidad;
+                totalVentas += int.Parse(matrizJuegos[index, 3]) * cantidad;
+
+                //DESCUENTOS
+                if (cantidad > 25 && matrizJuegos[index, 1] == "ACCIÓN")
+                {
+                    descuento = 15;
+                } else if (descuento < 10 && cantidad >= 32 && matrizJuegos[index, 0] == "FIFA" || cantidad > 12 && matrizJuegos[index, 1] == "DEPORTES" || cantidad > 15 && matrizJuegos[index, 1] == "VELOCIDAD")
+                {
+                    descuento = 10;
+                }
+            } else
+            {
+                MessageBox.Show("La cantidad solicitada no está dísponible");
+            }
+        }
+
+        private void btnFinalizarVenta_Click(object sender, EventArgs e)
+        {
+            btnComprar.Enabled = true;
+            btnFinalizarVenta.Enabled = false;
+
+            totalVentas = totalVentas - totalVentas * descuento / 100;
+            MessageBox.Show("El total a pagar es " + totalVentas + "\n con un descuento del " + descuento + "%");
+            totalVentas = 0;
+            descuento = 0;
+        }
+
+        private void btnMasVendido_Click(object sender, EventArgs e)
+        {
+            int mayor = vectorVentas[0];
+            int posicion = 0;
+
+            for (int i = 0; i < vectorVentas.Length; i++)
+            {
+                if (vectorVentas[i] > mayor)
+                {
+                    mayor = vectorVentas[i];
+                    posicion = i;
+                }
+            }
+
+            MessageBox.Show("El juego más vendido es " + matrizJuegos[posicion, 0]);
+        }
+
+        private void cmbJuego_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = cmbJuego.SelectedIndex;
+            txtNombre.Text = matrizJuegos[index, 0];
+            txtCategoria.Text = matrizJuegos[index, 1];
+            txtTamano.Text = matrizJuegos[index, 2];
+            txtPrecio.Text = matrizJuegos[index, 3];
+            txtDisponible.Text = matrizJuegos[index, 4];
+
+            switch(index)
+            {
+                case 0:
+                    pbJuego.Image = VideoGames.Properties.Resources.fifa;
+                    break;
+                case 1:
+                    pbJuego.Image = VideoGames.Properties.Resources.nfc;
+                    break;
+                case 2:
+                    pbJuego.Image = VideoGames.Properties.Resources.halo;
+                    break;
+                case 3:
+                    pbJuego.Image = VideoGames.Properties.Resources.minecraft;
+                    break;
+            }
+        }
+
+        private void CellGames_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < matrizJuegos.GetLength(0); i++)
+            {
+                cmbJuego.Items.Add(matrizJuegos[i, 0]);
+            }
+            cmbJuego.SelectedIndex = 0;
+        }
+    }
+}
